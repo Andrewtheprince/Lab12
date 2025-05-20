6a611bc41f4ff8a401742c09acbc4ac8f4f967ce
Andrewtheprince <andry.mazzo@outlook.com>
2025-05-20 12:02:58 +0200

    inizio lavoro

--- UI/controller.py
+++ UI/controller.py
@@ -10,7 +10,13 @@ class Controller:
         self._listCountry = []
 
     def fillDD(self):
-        pass
+        nazioni = self._model.getNazioni()
+        for nazione in nazioni:
+            self._view.ddcountry.options.append(ft.dropdown.Option(nazione))
+            self._listCountry.append(nazione)
+        self._listYear.extend([2015,2016,2017,2018])
+        for anno in self._listYear:
+            self._view.ddyear.options.append(ft.dropdown.Option(anno))
 
     def handle_graph(self, e):
         pass
--- database/DAO.py
+++ database/DAO.py
@@ -2,4 +2,19 @@ from database.DB_connect import DBConnect
 
 
 class DAO:
-    pass
+
+    @staticmethod
+    def getAllNazioni():
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary = True)
+        result = []
+        query = """ SELECT r.Country
+                    FROM go_retailers r
+                    group by r.Country
+                    order by r.Country asc"""
+        cursor.execute(query)
+        for row in cursor:
+            result.append(row["Country"])
+        cursor.close()
+        conn.close()
+        return result
